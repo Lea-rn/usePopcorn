f861132958e9f6b44fc83e8faa11614497445e3c
achrefmechergui <achreffcb123achref@gamil.com>
2025-12-21 14:42:27 +0100

    stars

--- usepopcorn/src/index.js
+++ usepopcorn/src/index.js
@@ -1,15 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom/client';
 // import './index.css';
 import App from './App';
 import StartRating from './StartRating';
+
+function Test () {
+  const [movieRating , setMovieRating] = useState(0)
+  return <div>
+<StartRating color='blue' defaultRating={3} onSetRating={setMovieRating}/>
+<p>This movie was rated {movieRating} stars</p>
+  </div> 
+}
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     {/* <App /> */}
-    <StartRating />
+
+    <StartRating
+    
+     color='red' 
+     size={70}
+      className="test"
+      maxRating={8}
+      messages = {["Terrible" , "Bad" , "Okay" , "Good" , "Amazing"]}
+      
+      />
+      <StartRating size={60} maxRating={10} color='purple' defaultRating={3} />
+      <Test/>
+  
   </React.StrictMode>
 );
 
